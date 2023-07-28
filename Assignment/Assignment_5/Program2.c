2e54f43782474d8e64df944ca7bce2fb0bb86011
Abhijeet Kalathil <99106689+Abhijeet-Kalathil-1000@users.noreply.github.com>
2023-07-29 01:36:14 +0530

    Add files via upload

---
+++ Assignment/Assignment_5/Program2.c
@@ -0,0 +1,41 @@
+///////////////////////////////////////////////////////////////////////
+//
+//  Program     : Write a program which writes stsuct in file
+//                and add stuct of student.
+//  Author      : Abhijeet Kalathil
+//  Created     : 28/07/2023   
+//
+///////////////////////////////////////////////////////////////////////
+#include<stdio.h>
+#include<stdlib.h>
+#include<fcntl.h>
+#include<unistd.h>
+#include<sys/stat.h>
+
+int main(int argc, char *argv[])
+{
+    struct stat sobj;
+
+    if(argc != 2)
+    {
+        printf("Insufficient arguments\n");
+        return -1;
+    }
+
+    stat(argv[1],&sobj);
+
+    if(S_ISREG(sobj.st_mode))
+    {
+        printf("Its a regular file\n");
+    }
+    else if(S_ISDIR(sobj.st_mode))
+    {
+        printf("Its a directory\n");
+    }
+    else if(S_ISLNK(sobj.st_mode))
+    {
+        printf("Its a symbolic link\n");
+    }
+
+    return 0;
+}
