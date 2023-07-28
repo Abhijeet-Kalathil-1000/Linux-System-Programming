d75272d59e422d35ba1bf6362e04923cf06dbf16
Abhijeet Kalathil <99106689+Abhijeet-Kalathil-1000@users.noreply.github.com>
2023-07-29 00:25:15 +0530

    Add files via upload

---
+++ Assignment/Assignment_2/Program2.c
@@ -0,0 +1,43 @@
+///////////////////////////////////////////////////////////////////////
+//
+//  Program     : Write a program which accepts fine name 
+//                and read all data from the file.
+//  Author      : Abhijeet Kalathil
+//  Created     : 28/07/2023   
+//
+///////////////////////////////////////////////////////////////////////
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<unistd.h>
+#include<fcntl.h>
+
+#pragma pack(1)
+struct Student
+{
+    int Rollno;
+    char Sname[20];
+    float Marks;
+    int Age;
+};
+
+int main(int argc, char *argv[])
+{
+    struct Student sobj;
+    char Fname[20];
+    int fd = 0;
+
+    sobj.Rollno = 11;
+    sobj.Marks = 89.99f;
+    sobj.Age = 25;
+    strcpy(sobj.Sname,"Rahul");
+
+    printf("Enter the file name : \n");
+    scanf("%s",Fname);
+
+    fd = creat(Fname,0777);
+
+    write(fd,&sobj,sizeof(sobj));
+
+    return 0;
+}
---
+++ Assignment/Assignment_2/Program3.c
@@ -0,0 +1,49 @@
+///////////////////////////////////////////////////////////////////////
+//
+//  Program     : Write a program which accepts directory name 
+//                and prints file name form the directory.
+//  Author      : Abhijeet Kalathil
+//  Created     : 28/07/2023   
+//
+///////////////////////////////////////////////////////////////////////
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<unistd.h>
+#include<fcntl.h>
+#include<dirent.h>
+#include<sys/stat.h>
+
+int main(int argc, char *argv[])
+{
+    char DirName[20];
+    DIR *dp = NULL;
+    struct dirent *entry = NULL;
+    char name[30];
+    struct stat sobj;
+
+    printf("Enter name of directory : \n");
+    scanf("%s",DirName);
+
+    dp = opendir(DirName);
+    if(dp == NULL)
+    {
+        printf("Unable to open directory\n");
+        return -1;
+    }
+
+    while((entry = readdir(dp)) != NULL)
+    {
+        sprintf(name,"%s/%s",DirName,entry->d_name);
+        stat(name,&sobj);
+        if(S_ISREG(sobj.st_mode))
+        {
+           printf("File name : %s ",name);
+        }
+    }
+
+    closedir(dp);
+    
+    return 0;
+}
